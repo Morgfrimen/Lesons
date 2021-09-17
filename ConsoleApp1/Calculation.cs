e3f1a4745f34c3b90e1cf00c30c39d0568da4baa
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-09-18 00:08:02 +0300

    Add new logical

--- ConsoleApp1/Calculation.cs
+++ ConsoleApp1/Calculation.cs
@@ -8,5 +8,11 @@ namespace ConsoleApp1
 	internal class Calculation
 	{
 		internal T Sum<T>(T one, T two) where T : struct => Convert.ChangeType((dynamic)one + (dynamic)two, typeof(T));
+
+		internal T Different<T>(T value1, T value2)	where T : struct => Convert.ChangeType((dynamic)value1 - (dynamic)value2, typeof(T));
+
+		internal T Subdivision<T>(T value1, T value2)  where T : struct => Convert.ChangeType((dynamic)value1 / (dynamic)value2, typeof(T));
+
+		internal T Multiplication<T>(T value1, T value2) where T : struct => Convert.ChangeType((dynamic)value1 * (dynamic)value2, typeof(T));
 	}
 }
