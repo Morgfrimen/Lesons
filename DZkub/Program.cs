9787801bd3361daa07dc3738ccfe4c84c2629dcb
Артём Крупенников <krupennikov.artem@gmail.com>
2021-09-27 21:00:02 +0300

    Добавил дз

---
+++ DZkub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool error = true;
+            while (error)
+            {
+                try
+                {
+                    Console.WriteLine("Введите число");
+                    double chislo = Convert.ToInt32(Console.ReadLine());
+                    chislo = chislo * chislo * chislo;
+                    Console.WriteLine($"Это число в кубе: {chislo}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка!");
+                    
+                }
+            }
+        }
+    }
+}
