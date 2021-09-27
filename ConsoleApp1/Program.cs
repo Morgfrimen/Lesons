0ec39c4c53828690221254df869103755ef7c81a
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-09-27 21:14:25 +0300

    fix net 5.0

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 
 using ConsoleApp1.Extension;
 
 [assembly: InternalsVisibleTo("UnitTest")]
 
-namespace ConsoleApp1;
-
-
-
-public class Program
+namespace ConsoleApp1
 {
-	private static void Main(string[] args)
-	{
-		int value = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine(value.GetKube(new List<CustomNumber>()));
-	}
 
-	public struct CustomNumber
-	{
-		public double Value { get; set; }
 
-		public static implicit  operator double (CustomNumber customNumber)
+	public class Program
+	{
+		private static void Main(string[] args)
 		{
-			return customNumber.Value;
+			int value = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(value.GetKube(new List<CustomNumber>()));
 		}
-	}
 
+		public struct CustomNumber
+		{
+			public double Value { get; set; }
+
+			public static implicit operator double(CustomNumber customNumber)
+			{
+				return customNumber.Value;
+			}
+		}
+
+	}
 }
