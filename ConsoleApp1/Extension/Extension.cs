684b0bccf26e3cd06f0e4c4730511ba85b6ed50d
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-09-27 20:47:26 +0300

    Add extension

---
+++ ConsoleApp1/Extension/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1.Extension
+{
+	internal static class Extension
+	{
+		public static T GetKube<T, Tout>(this T value, Tout str) where T : struct
+																		where Tout : class
+		{
+			if (str is string)
+				Console.WriteLine(str);
+
+			var result = Convert.ChangeType((double)((dynamic)value * (dynamic)value * (dynamic)value), typeof(T));
+			return (T)result;
+		}
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using ConsoleApp1.Extension;
 
 [assembly: InternalsVisibleTo("UnitTest")]
 
@@ -10,7 +16,18 @@ public class Program
 {
 	private static void Main(string[] args)
 	{
+		int value = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine(value.GetKube(new List<CustomNumber>()));
+	}
 
+	public struct CustomNumber
+	{
+		public double Value { get; set; }
+
+		public static implicit  operator double (CustomNumber customNumber)
+		{
+			return customNumber.Value;
+		}
 	}
 
 }
