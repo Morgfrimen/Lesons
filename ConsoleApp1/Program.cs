a1ff6adaffc4965e6cb7811e8e0a8c12767a71d1
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-10-09 16:58:21 +0300

    пример для решения задач

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,41 +9,17 @@ namespace ConsoleApp1
 	{
 		private static void Main(string[] args)
 		{
-			decimal test = 10;
-			long tast1 = -1;
-			string ret = "hi";
+			Console.WriteLine("Введи возраст");
+			string valueInput = Console.ReadLine();
 
-			int aA = 22;
-			int b = 300;
-			int resultSum = aA + b;
-			int resultMinus = aA * b;
-			int q1 = 20;
-			int q2 = 10;
-			int q3 = q1 + q2;
-			int q4 = q1 - q2;
-			int q5 = q1 * q2;
-			int q6 = q1 / q2;
-
-			double q7 = 10;
-			double q8 = 6;
-			double q9 = q7 / q8;
-
-			double q10 = 12.0;
-			double q11 = 5.0;
-			double q12 = q10 % q11;
-
-			
-
-
-
-
-
-			
-
-
-
-			
-
+			if (int.TryParse(valueInput,out int result) && result >= 0 && result <= 100)
+			{
+				Console.WriteLine(result);
+			}
+			else
+			{
+				Console.WriteLine("Ошибка");
+			}
 		}
 	}
 }
